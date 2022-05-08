2b60d083f2d2e184031890cc11ba85e542c386c6
ccbagel <hasan.abdi.ahmed97@gmail.com>
2022-05-08 09:43:19 -0600

    hero section completed

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -8,10 +8,10 @@ function HeroBanner({ heroBanner }) {
     return (
         <div className="flex flex-col md:flex-row">
             <div className="black-bg flex flex-col md:items-center w-full md:w-1/2 p-10 md:p-20">
-                <div>
+                <div className="">
                     <p className="text-sm font-thin text-white my-2">Featured - Dior Homme</p>
                     <h3 className="text-3xl md:text-4xl light-brown-text my-2">{heroBanner.midText}</h3>
-                    <h1 className="text-6xl md:text-9xl uppercase text-white my-2">{heroBanner.largeText1} %</h1>
+                    <h1 className="text-7xl md:text-9xl uppercase text-white my-4">{heroBanner.largeText1}%</h1>
                      <Link href={`/product/${heroBanner.product}`}>
                         <button type="button" className="light-brown-bg my-2 p-3 text-xs uppercase tracking-widest font-semibold">{heroBanner.buttonText}</button>
                     </Link>
@@ -31,8 +31,8 @@ function HeroBanner({ heroBanner }) {
                 </div> */}
             </div>
             <div className="w-full md:w-1/2 relative">
-                <div className="w-full h-full">
-                    <Image src={random} alt="fragrance" objectFit="cover" layout="fill" />
+                <div className="w-full h-full mt-[499px] md:mt-0">
+                    <Image src={random} alt="fragrance" objectFit="cover" layout="fill" width={100} height={100} />
                 </div>
             </div>
         </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <div className="black-bg white-text flex flex-row justify-between items-center md:px-20 p-7 w-full">
             <div>
-                <h1 className="text-4xl">Scentify</h1>
+                <h1 className="text-2xl md:text-4xl">Scentify</h1>
             </div>
             <div className="hidden md:block">
                 <ul className="flex flex-row justify-between items-center">
